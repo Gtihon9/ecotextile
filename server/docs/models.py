bf406b88721c468cd8636e4e94fc28ff920bbe8c
Alexander Kovalevich <87308032+1terator@users.noreply.github.com>
2022-02-01 23:11:15 +0300

    adding docs app
    app working with files

---
+++ server/docs/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Document(models.Model):
+    title = models.CharField(max_length=50)
+    file = models.FileField(upload_to="docs")
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return self.title
